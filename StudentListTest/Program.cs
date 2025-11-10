cf472abd4975f5a43dd25f11e07527b2fa56d118
Kiphea <djsmyth05@gmail.com>
2025-11-10 03:46:37 +0000

    Challenge 4 Complete - Task 5 Finished

--- StudentListTest/Program.cs
+++ StudentListTest/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        string studentName;
+        int studentAge;
+        List<Student> studentList = new List<Student>();
 
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Please enter the student's name: ");
+            studentName = Console.ReadLine();
+
+            Console.WriteLine("Please enter the student's age: ");
+            studentAge = Convert.ToInt32(Console.ReadLine());
+
+            studentList.Add(new Student(studentName, studentAge));
+        }
+
+        foreach (Student student in studentList)
+        {
+            student.DisplayInfo();
+        }
     }
 }
