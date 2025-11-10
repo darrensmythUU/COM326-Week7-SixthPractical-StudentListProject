5626b67fe3d48c093819790421b4a22f6df84b02
Kiphea <djsmyth05@gmail.com>
2025-11-10 03:36:34 +0000

    Task 2 Complete

--- SixthPractical-StudentListProject/Student.cs
+++ SixthPractical-StudentListProject/Student.cs
@@ -2,6 +2,57 @@
 {
     public class Student
     {
+        // Private Fields
+        private int id;
+        private string name;
+        private int age;
+        private static int studentCount = 0;
 
+        // Get/Set Constructors
+        public int ID
+        {
+            get { return id; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public static int StudentCount
+        {
+            get { return studentCount; }
+        }
+
+        // Constructors for Student object
+        public Student()
+        {
+            id = studentCount++;
+            this.Name = "John Doe";
+            this.Age = 16;
+        }
+
+        public Student(string studentName, int studentAge)
+        {
+            id = studentCount++;
+            this.Name = studentName;
+            this.Age = studentAge;
+        }
+
+        // Methods
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Student ID: {ID}");
+            Console.WriteLine($"Student Name: {Name}");
+            Console.WriteLine($"Student Address: {Age}");
+            Console.WriteLine();
+        }
     }
 }
